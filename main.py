0a5a6a13f44a01f61eb395605696e81ebfa1cf42
hackerSploit <ruanvsd2024@gmail.com>
2024-07-30 00:05:38 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-def ajuda ():
-    print("qualquer ajuda é bem vinda")
-resposta = input("me da ajuda: : ")
-if resposta == "sim":
-    print("hello world :)")
-else:
-    print("hello world :(")
-    
