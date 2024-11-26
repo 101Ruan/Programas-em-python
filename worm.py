b1b46792f6c9fc76e360f1ad2f36644cd02efa13
hackerSploit <ruanvsd2024@gmail.com>
2024-11-26 01:07:42 -0300

    Update worm.py

--- worm.py
+++ worm.py
@@ -1,5 +1,8 @@
 import os
 i = 0
 while True:
-	open(f"argv{i}.txt", "w").write("worm detected!")
+	nome = input("qual é seu nome? ")
+	print(f"ok {nome}, espere 5 segundos
+	open(f"hacked{i}.txt", "w").write("worm detected!")
 	i += 1
+	
