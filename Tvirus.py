e0dd2b039277f56e750cdcb89dfef1a05b4fdc64
hackerSploit <ruanvsd2024@gmail.com>
2025-06-17 15:51:42 -0300

    Update Tvirus.py

--- Tvirus.py
+++ Tvirus.py
@@ -1,14 +1,33 @@
 from colorama import init, Fore, Style
 import os
+import sys
+import pyfiglet
 
 init()
 
-virus_achados = input(Fore.RED + "- Nome do arquivo: " + Style.RESET_ALL)
+def limpar():
+    os.system("cls")
+    if sys.platform == "linux":
+       os.system("clear") 
+limpar()
 
+def logo():
+    texto = "Tvirus"
+    figlet = pyfiglet.figlet_format(texto)
+    print(figlet)
+logo()
+
+try:
+    virus_achados = input(Fore.RED + "- Nome do arquivo: " + Style.RESET_ALL)
+except KeyboardInterrupt:
+    print("\n")
+    print(Fore.RED+"saindo..."+Style.RESET_ALL)
+    exit()
+    
 try:
     with open(virus_achados, "r") as file:
         conteudo = file.read()
-        malwares_nomes = ["worm", "trojan", "ramsomware", "spyware"]
+        malwares_nomes = ["worm", "virus", "trojan", "ramsomware", "spyware", "rat", "backdoor", "criptojacking"]
         achados = [nome for nome in malwares_nomes if nome in conteudo.lower()]
         
         if achados:
@@ -21,6 +40,7 @@ except FileNotFoundError:
     print(Fore.RED + f"- Arquivo '{virus_achados}' não encontrado." + Style.RESET_ALL)
 except PermissionError:
     print(Fore.RED + f"- Acesso negado ao arquivo '{virus_achados}'." + Style.RESET_ALL)
-except Exception as e:
-    print(Fore.RED + f"- Erro: {str(e)}" + Style.RESET_ALL)
+except KeyboardInterrupt:
+    print(Fore.RED + f"- você saiu" + Style.RESET_ALL)
+    exit()
     
